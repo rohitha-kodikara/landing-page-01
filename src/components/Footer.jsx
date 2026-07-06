99b237c66852f1ccd82324e51439b74a51035f5e
S.K.ROHITHA <rohithasamarawickrama@gmail.com>
2026-07-06 11:20:28 +0530

    adding some padding for footer details.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-blue-200 mt-12 px-12 md:px-10 grid grid-cols-2 md:grid-cols-3  gap-10 items-start pb-8">
-      <div className="flex flex-col gap-2 pt-5">
+      <div className="flex flex-col gap-2 pt-8 md:mt-10">
         <h3 className="font-bold text-blue-900 font-sans">Contact</h3>
         <span>Tell Us Everything</span>
         <span>Do you have any question ?</span>
@@ -11,13 +11,13 @@ const Footer = () => {
 
         <span className="text-2xl">Let's Chat</span>
       </div>
-      <div className="flex flex-col gap-2 pt-5">
+      <div className="flex flex-col gap-2 pt-5  md:mt-10">
         <h3 className="font-bold text-blue-900 font-sans">Policy</h3>
         <span>Application Security</span>
         <span>Software Principles</span>
       </div>
 
-      <div className="flex flex-col gap-2 pt-5">
+      <div className="flex flex-col gap-2 pt-5  md:mt-10">
         <span>Support Center</span>
         <span>Customer Suport</span>
       </div>
